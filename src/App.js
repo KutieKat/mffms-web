1ea2fddb6e422cc1289008c32e0e210350eb3515
kutiekat <kutiekat0318@gmail.com>
2019-12-02 00:02:09 +0700

    Update app logo

--- src/App.js
+++ src/App.js
@@ -83,7 +83,14 @@ class App extends Component {
       <Fragment>
         <header className="main-header">
           <div className="main-header__left">
-            <h1 className="main-header__title">MFFMS</h1>
+            <h1 className="main-header__title">
+              <img
+                src="/images/logo.png"
+                alt="Logo"
+                className="main-header__logo"
+              />
+              MFFMS
+            </h1>
             <p className="main-header__subtitle">
               Mini Football Field Management System
             </p>
